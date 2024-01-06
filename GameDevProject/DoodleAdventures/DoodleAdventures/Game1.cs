fbfcd99fa3736d2bbf19dbab788cb6b415acc5a1
KoenVA <koenvanaken1999@gmail.com>
2024-01-06 14:03:03 +0100

    Draw Hero

--- GameDevProject/DoodleAdventures/DoodleAdventures/Game1.cs
+++ GameDevProject/DoodleAdventures/DoodleAdventures/Game1.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Vector2 = System.Numerics.Vector2;
 
 namespace DoodleAdventures;
 
@@ -10,6 +11,8 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private Texture2D _background;
+    private Hero _hero;
+    private Dictionary<string,Texture2D> _heroTextures;
 
     public Game1()
     {
@@ -27,6 +30,14 @@ public class Game1 : Game
         _graphics.ApplyChanges();
 
         base.Initialize();
+
+        _hero = new Hero(
+            _heroTextures["head"], 
+            _heroTextures["torso"],
+            _heroTextures["armR"],
+            _heroTextures["armL"],
+            _heroTextures["legR"],
+            _heroTextures["legL"]);
     }
 
     protected override void LoadContent()
@@ -35,6 +46,16 @@ public class Game1 : Game
 
         // TODO: use this.Content to load your game content here
         _background = Content.Load<Texture2D>("sqrd_grid_paper");
+
+        _heroTextures = new Dictionary<string, Texture2D>()
+        {
+            {"head", Content.Load<Texture2D>("head")},
+            {"torso", Content.Load<Texture2D>("torso")},
+            {"armR", Content.Load<Texture2D>("right_arm")},
+            {"armL", Content.Load<Texture2D>("left_arm")},
+            {"legR", Content.Load<Texture2D>("right_leg")},
+            {"legL", Content.Load<Texture2D>("left_leg")}
+        };
     }
 
     protected override void Update(GameTime gameTime)
@@ -55,6 +76,7 @@ public class Game1 : Game
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
         _spriteBatch.Draw(_background,Vector2.Zero, new Rectangle(0,0,640,480),Color.White);
+        _hero.Draw(_spriteBatch);
         _spriteBatch.End();
 
         base.Draw(gameTime);
---
+++ GameDevProject/DoodleAdventures/DoodleAdventures/Hero.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DoodleAdventures;
+
+public class Hero : IGameObject
+{
+    private Texture2D _head, _torso, _armR, _armL, _legR, _legL;
+    private Vector2 _position;
+
+    public Hero(Texture2D head, Texture2D torso, Texture2D armR, Texture2D armL, Texture2D legR, Texture2D legL)
+    {
+        _head = head;
+        _torso = torso;
+        _armR = armR;
+        _armL = armL;
+        _legR = legR;
+        _legL = legL;
+
+        _position = new Vector2(100, 100);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_torso, _position, Color.White);
+        spriteBatch.Draw(_head,_position-new Vector2(0,40),Color.White);
+        spriteBatch.Draw(_armL, _position-new Vector2(40,0),Color.White);
+        spriteBatch.Draw(_armR, _position+new Vector2(40,0),Color.White);
+        spriteBatch.Draw(_legL, _position+new Vector2(0,40),Color.White);
+        spriteBatch.Draw(_legR, _position+new Vector2(20,40),Color.White);
+    }
+}
